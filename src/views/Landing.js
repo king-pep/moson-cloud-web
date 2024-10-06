cffb2347ffc7471d8505ebea888b4bcf6fd766ed
AbelBen <abelmorrisben@icloud.com>
2024-10-06 19:11:03 +0200

    first commit

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -144,7 +144,7 @@ export default function Landing() {
                                     className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-lightBlue-500">
                                     <img
                                         alt="..."
-                                        src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
+                                        src="https://images.unsplash.com/photo-1573496358488-51c188554a47?q=80&w=3269&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                                         className="w-full align-middle rounded-t-lg"
                                     />
                                     <blockquote className="relative p-8 mb-4">
