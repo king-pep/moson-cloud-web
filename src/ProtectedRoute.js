2011a1f735452a2e1555398760168b01040421db
AbelBen <abelmorrisben@icloud.com>
2024-10-07 15:42:31 +0200

    first commit

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { redirectToLogin } from './redirectToLogin';
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
