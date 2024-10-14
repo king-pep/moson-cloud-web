ae7745444ee85b7639c429d688a887b23de34172
king-pep <abelmorrisben@icloud.com>
2024-10-14 21:22:30 +0200

    Update tokenService.js

--- src/tokenService.js
+++ src/tokenService.js
@@ -1,7 +1,7 @@
 export const exchangeCodeForTokens = async (authorizationCode) => {
     const tokenUrl = 'https://alpha.mosontech.co.za/realms/db-manager/protocol/openid-connect/token';
     const clientId = 'docker-db-inst';
-    const redirectUri = 'https://alpha.mosontech.co.za/callback';
+    const redirectUri = 'https://mosoncloud.mosontech.co.za/callback';
     const clientSecret = 'pXFYAnX1I6XUIxxZ5biDqRNalALHEUQl';
 
 
