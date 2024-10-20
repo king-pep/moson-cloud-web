31f6f11901a0f2093108a202fd8102c9878be309
AbelBen <abelmorrisben@icloud.com>
2024-10-20 08:55:53 +0200

    Add ApiClient.js

--- src/RedirectToLogin.js
+++ src/RedirectToLogin.js
@@ -1,7 +1,7 @@
 export const redirectToLogin = () => {
     const keycloakUrl = 'https://alpha.mosontech.co.za/realms/db-manager/protocol/openid-connect/auth';
     const clientId = 'docker-db-inst';
-    const redirectUri = encodeURIComponent(process.env.REACT_APP_REDIRECT_URI);
+    const redirectUri = encodeURIComponent('https://mosoncloud.mosontech.co.za/callback');
     const responseType = 'code';
 
     window.location.href = `${keycloakUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}`;
